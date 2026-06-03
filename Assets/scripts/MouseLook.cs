2dae119e5c83dd7e0ef779d2608f90f075a91450
yuma12sa <yuma12sa@gmail.com>
2026-06-03 16:59:45 +0900

    [update]MouseLookスクリプトにコメントアウトを追加

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 class MouseLook : MonoBehaviour
 {
-    [SerializeField] float mouseSensitivity = 0.1f;
+    [SerializeField] float mouseSensitivity = 0.1f;//マウス感度
     [SerializeField] Transform playerBody;
 
     private InputAction lookAction;
@@ -13,22 +13,21 @@ class MouseLook : MonoBehaviour
     private void Start()
     {
         lookAction = InputSystem.actions.FindAction("Look");
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;//マウスカーソルを中央に固定
     }
 
     private void Update()
     {
         Vector2 lookValue = lookAction.ReadValue<Vector2>();
 
-        float mouseX = lookValue.x * mouseSensitivity;
+        float mouseX = lookValue.x * mouseSensitivity;//マウス入力の取得
         float mouseY = lookValue.y * mouseSensitivity;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;//上下反転
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);//角度制限
 
-        transform.localRotation =
-            Quaternion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);//上下回転処理
 
-        playerBody.Rotate(Vector3.up * mouseX);
+        playerBody.Rotate(Vector3.up * mouseX);//プレイヤー左右回転
     }
 }
