3f431c639878d8f4494d05e7eeabd78585fa2865
yuma12sa <yuma12sa@gmail.com>
2026-06-10 17:43:25 +0900

    [update]PlayerControllerでインプットシステムに対応した移動とジャンプが出来るように

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -3,31 +3,31 @@ using UnityEngine.InputSystem;
 
 class MouseLook : MonoBehaviour
 {
-    [SerializeField] float mouseSensitivity = 0.1f;//マウス感度
-    [SerializeField] Transform playerBody;
+    [SerializeField] float _mouseSensitivity = 0.1f;//マウス感度
+    [SerializeField] Transform _playerBody;
 
-    private InputAction lookAction;
+    private InputAction _lookAction;
 
-    private float xRotation = 0f;
+    private float _xRotation = 0f;
 
     private void Start()
     {
-        lookAction = InputSystem.actions.FindAction("Look");
+        _lookAction = InputSystem.actions.FindAction("Look");
         Cursor.lockState = CursorLockMode.Locked;//マウスカーソルを中央に固定
     }
 
     private void Update()
     {
-        Vector2 lookValue = lookAction.ReadValue<Vector2>();
+        Vector2 lookValue = _lookAction.ReadValue<Vector2>();
 
-        float mouseX = lookValue.x * mouseSensitivity;//マウス入力の取得
-        float mouseY = lookValue.y * mouseSensitivity;
+        float mouseX = lookValue.x * _mouseSensitivity;//マウス入力の取得
+        float mouseY = lookValue.y * _mouseSensitivity;
 
-        xRotation -= mouseY;//上下反転
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);//角度制限
+        _xRotation -= mouseY;//上下反転
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);//角度制限
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);//上下回転処理
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);//上下回転処理
 
-        playerBody.Rotate(Vector3.up * mouseX);//プレイヤー左右回転
+        _playerBody.Rotate(Vector3.up * mouseX);//プレイヤー左右回転
     }
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,6 +1,68 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private float _moveSpeed = 3.0f;
+    [SerializeField] private float _jumpPower = 3.0f;
+
+    private bool _isGrounded;
+
+    [SerializeField] private PlayerInput _playerInput;
+
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+
+    private Rigidbody _rb;
+
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        _moveAction = _playerInput.actions["Move"];
+        _jumpAction = _playerInput.actions["Jump"];
+
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _moveInput = _moveAction.ReadValue<Vector2>();
+        
+        if (_jumpAction.triggered)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move(_moveInput);
+    }
+
+    private void Move(Vector2 input)
+    { 
+        Vector3 moveDirection = transform.right * input.x + transform.forward * input.y;
+
+        Vector3 velocity = new Vector3(moveDirection.x * _moveSpeed, _rb.linearVelocity.y,moveDirection.z * _moveSpeed);
+
+        _rb.linearVelocity = velocity;
+    }
+
+    private void Jump()
+    {
+        if (!_isGrounded) return;
+
+        _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        _isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
 }
