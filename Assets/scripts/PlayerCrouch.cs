5a9d194afc238132e42ddd75ac5816017a9ab43a
yuma12sa <yuma12sa@gmail.com>
2026-06-11 20:26:09 +0900

    [update]PlayerCrouchでカプセルコライダーの大きさをいじれるように変更

--- Assets/scripts/PlayerCrouch.cs
+++ Assets/scripts/PlayerCrouch.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerCrouch : MonoBehaviour
+class PlayerCrouch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _usuallySize = 2.0f;
+    [SerializeField] private float _CrouchSize = 1.0f;
+
+    [SerializeField] private PlayerInput _playerInput;
+
+    private InputAction _crouchAction;
+
+    private CapsuleCollider _capsuleCol;
+
+
+    private void Awake()
     {
-        
+        _crouchAction = _playerInput.actions["Crouch"];
+
+        _capsuleCol = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_crouchAction.IsPressed())
+        {
+            _capsuleCol.height = _CrouchSize;
+        }
+        else
+        {
+            _capsuleCol.height = _usuallySize;
+        }
     }
 }
