4b4843807e24d4843ddc631a6cb108720fba69e7
yuma12sa <yuma12sa@gmail.com>
2026-07-09 19:20:24 +0900

    [update]PlayerCrouchでしゃがんだ時にカメラの位置が変わるように変更

--- Assets/scripts/PlayerCrouch.cs
+++ Assets/scripts/PlayerCrouch.cs
@@ -5,8 +5,12 @@ class PlayerCrouch : MonoBehaviour
 {
     [SerializeField] private float _usuallySize = 2.0f;
     [SerializeField] private float _crouchSize = 1.0f;
+    [SerializeField] private float _usuallyCamera = 1.0f;
+    [SerializeField] private float _crouchCamera = 0.5f;
 
     [SerializeField] private PlayerInput _playerInput;
+
+    public Camera _camera;
 
     private InputAction _crouchAction;
 
@@ -26,11 +30,15 @@ class PlayerCrouch : MonoBehaviour
         {
             _capsuleCol.height = _crouchSize;
             _capsuleCol.center = new Vector3(0, _crouchSize / 2, 0);
+
+            _camera.transform.localPosition = new Vector3(0, _crouchCamera, 0);
         }
         else
         {
             _capsuleCol.height = _usuallySize;
             _capsuleCol.center = new Vector3(0, _usuallySize / 2, 0);
+
+            _camera.transform.localPosition = new Vector3(0, _usuallyCamera, 0);
         }
     }
 }
