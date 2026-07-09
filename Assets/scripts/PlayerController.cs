df2e0f84b2acb25bd6c09b3ac755f507ddf56fec
yuma12sa <yuma12sa@gmail.com>
2026-07-09 21:05:40 +0900

    [update]プレイヤーコントローラーにローリング処理を追加途中

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 class PlayerController : MonoBehaviour
 {
@@ -7,6 +9,7 @@ class PlayerController : MonoBehaviour
     [SerializeField] private float _moveSpeed = 3.0f;
     [SerializeField] private float _dashSpeed = 10.0f;
     [SerializeField] private float _jumpPower = 3.0f;
+    [SerializeField] private float _rollingSpeed = 20.0f;
 
     [Header("‚µ‚á‚ª‚Ý")]
     [SerializeField] private float _usuallySize = 2.0f;
@@ -22,6 +25,7 @@ class PlayerController : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private InputAction _dashAction;
+    private InputAction _rollingAction;
 
     private Rigidbody _rb;
     private CapsuleCollider _capsuleCol;
@@ -29,12 +33,15 @@ class PlayerController : MonoBehaviour
     private Vector2 _moveInput;
     private bool _isGrounded;
 
+    private bool _canMove;
+
     private void Awake()
     {
         _moveAction = _playerInput.actions["Move"];
         _dashAction = _playerInput.actions["Dash"];
         _jumpAction = _playerInput.actions["Jump"];
         _crouchAction = _playerInput.actions["Crouch"];
+        _rollingAction = _playerInput.actions["Rolling"];
 
         _rb = GetComponent<Rigidbody>();
         _capsuleCol = GetComponent<CapsuleCollider>();
@@ -43,6 +50,7 @@ class PlayerController : MonoBehaviour
     private void Start()
     {
         _isGrounded = true;
+        _canMove = true;
     }
 
     private void Update()
@@ -55,7 +63,12 @@ class PlayerController : MonoBehaviour
             Jump();
         }
 
-        if (_crouchAction.IsPressed())
+        if (_rollingAction.triggered)
+        {
+            StartCoroutine(Rolling());
+        }
+
+        if (_crouchAction.IsPressed() && _canMove)
         {
             _capsuleCol.height = _crouchSize;
             _capsuleCol.center = new Vector3(0, _crouchSize / 2, 0);
@@ -74,8 +87,12 @@ class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
-        Move(_moveInput);
+    { 
+        if (_canMove)
+        {
+            Move(_moveInput);
+        }
+        
     }
 
     private void Move(Vector2 input)
@@ -91,12 +108,7 @@ class PlayerController : MonoBehaviour
             currentSpeed = _moveSpeed;
         }
 
-        Vector3 moveDirection = transform.right * input.x + transform.forward * input.y;
-
-        if (moveDirection.sqrMagnitude > 1f)
-        {
-            moveDirection.Normalize();
-        }
+        Vector3 moveDirection = GetMoveDirection(input);
 
         Vector3 velocity = new Vector3(moveDirection.x * currentSpeed, _rb.linearVelocity.y,moveDirection.z * currentSpeed);
 
@@ -106,10 +118,54 @@ class PlayerController : MonoBehaviour
     private void Jump()
     {
         if (!_isGrounded) return;
+        if (!_canMove) return;
 
         _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
         _isGrounded = false;
     }
+
+    private IEnumerator Rolling()
+    { 
+        _canMove = false;
+
+        Vector3 rollDirection;
+        if (_moveInput.sqrMagnitude > 0.01f)
+        {
+            rollDirection = GetMoveDirection(_moveInput);
+        }
+        else
+        {
+            rollDirection = transform.forward;
+        }
+
+        float rollTime = 0.3f;
+        float timer = 0f;
+        
+        while (timer < rollTime)
+        { 
+            _rb.linearVelocity = new Vector3(rollDirection.x * _rollingSpeed, _rb.linearVelocity.y, rollDirection.z * _rollingSpeed);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        _canMove = true;
+    }
+
+
+
+    private Vector3 GetMoveDirection(Vector2 input)
+    {
+        Vector3 moveDirection = transform.right * input.x + transform.forward * input.y;
+
+        if (moveDirection.sqrMagnitude > 1f)
+        {
+            moveDirection.Normalize();
+        }
+
+        return moveDirection;
+    }
+
 
     private void OnCollisionEnter(Collision other)
     {
